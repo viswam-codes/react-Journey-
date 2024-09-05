3b9f31422f356762c4cf22437154d7f82ff402b1
viswam-codes <jithviswa24@gmail.com>
2024-09-05 21:56:14 +0530

    Food Delivery app practice

--- app.js
+++ app.js
@@ -3,45 +3,76 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 /**
- * <div id=parent>
- * <div id=child>
- * <h1>I am an h1 tag</h1>
- * <h2>I am an h2 tag </h2>
- * </div>
- * </div>
- */
-const header = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ])
-);
-
-/**
- * <div id=parent>
- * <div id=child>
- * <h1>I am an h1 tag</h1>
- * <h2>I am an h2 tag </h2>
- * </div>
- * <div id=child2>
- * <h1>I am an h1 tag</h1>
- * <h2>I am an h2 tag </h2>
- * </div>
- * </div>
+ * header
+ * -logo
+ * -Nav items
+ * body
+ * -search
+ * -Restaurant Container
+ * -Resstaurant Card
+ *    -Img
+ *    -Name of res,Star rating,cuisine ,delivery time etc
+ * footer
+ * -copyright
+ * -links
+ * -addresss
+ * -contact
  */
 
-const header2 = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 nested tag"),
-    React.createElement("h2", {}, "I am an h2 nested tag"),
-  ]),
-]);
+const Header = ()=>{
+  return (
+    <div className='header'>
+      <div className='logo-container'>
+          <img  className="logo" src='https://mir-s3-cdn-cf.behance.net/project_modules/1400/31858f64574437.5ad6dd7253bb4.jpg' />
+      </div>
+      <div className='nav-items'>
+        <ul className='navList'>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
+
+const RestaurantCards=(props)=>{
+  return (
+    <div className='res-Card' style={{backgroundColor:"#f0f0f0"}}>
+      <img className='res-logo' src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/92e2a4ce7f38b751e16c098d1d611a3f' />
+      <h3>{props.resName}</h3>
+      <h4>Biriyani,North Indian,Chinese</h4>
+      <h6>4.4 stars</h6>
+      <h6>38 minutes</h6>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+  <div className='body'>
+    <div className='search'></div>
+    <div className='res-container'>
+      < RestaurantCards resName="Arabian Fiesta" />
+      < RestaurantCards resName="KFC" />
+    </div>
+  </div>
+  )
+}
+
+
+
+const AppLayout = ()=>{
+  return (
+    <div className="app">
+      < Header />
+      < Body />
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(header2);
+root.render(< AppLayout />)
